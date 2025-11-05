38a31c930d7aa5b6139551c0d4259fcb241461da
Luiz Rodrigues <atende@webvitua.com.br>
2025-11-05 18:21:49 -0300

    refactor: customizing header

--- src/custom-extensions-coreui/components/CoreUILayout.tsx
+++ src/custom-extensions-coreui/components/CoreUILayout.tsx
@@ -1,8 +1,9 @@
 import { Suspense, ReactNode } from 'react';
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '@easy-message/core-ui/components';
+import { AppContent, AppSidebar, AppFooter } from '@easy-message/core-ui/components';
 import { CContainer, CSpinner } from '@coreui/react';
 import { Route, Routes } from 'react-router-dom';
 import navigation from '@easy-message/core-ui/_nav';
+import CustomAppHeader from './CustomAppHeader'; // Import the custom header
 
 interface RouteType {
   path: string;
@@ -22,7 +23,7 @@ const CoreUILayout = ({ routes, header, footer }: Props) => {
     <div>
       <AppSidebar navigation={navigation} />
       <div className="wrapper d-flex flex-column min-vh-100">
-        {header || <AppHeader />}
+        {header || <CustomAppHeader />}
         <div className="body flex-grow-1 px-3">
           <AppContent>
             <Suspense fallback={<CContainer lg><CSpinner color="primary" /></CContainer>}>
